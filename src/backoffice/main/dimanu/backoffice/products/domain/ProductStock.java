a10fe4c5b389dd32e3148177756936dc7ae6a17e
dimanu-py <diegomtz126@gmail.com>
2024-11-30 17:47:33 +0100

    feat(backoffice): create ProductStock value object

---
+++ src/backoffice/main/dimanu/backoffice/products/domain/ProductStock.java
@@ -0,0 +1,9 @@
+package dimanu.backoffice.products.domain;
+
+import dimanu.shared.domain.IntegerValueObject;
+
+public class ProductStock extends IntegerValueObject {
+    public ProductStock(int value) {
+        super(value);
+    }
+}
