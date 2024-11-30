9863b2c48690f8f154f80a01547e0fa60183ed8c
dimanu-py <diegomtz126@gmail.com>
2024-11-30 02:09:35 +0100

    feat(app-backoffice): implement health check controller

---
+++ app/main/dimanu/app/backoffice/healthcheck/HealthCheckGetController.java
@@ -0,0 +1,21 @@
+package dimanu.app.backoffice.healthcheck;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
+@RestController
+@RequestMapping("/health-check")
+public class HealthCheckGetController {
+
+    @GetMapping
+    public ResponseEntity<HashMap<String, String>> healthCheck() {
+        HashMap<String, String> response = new HashMap<>();
+        response.put("status", "Healthy");
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+}
---
+++ app/test/dimanu/app/backoffice/healthcheck/HealthCheckGetControllerTest.java
@@ -0,0 +1,47 @@
+package dimanu.app.backoffice.healthcheck;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class HealthCheckGetControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+
+    @Test
+    void should_check_application_health() {
+        int expectedStatusCode = 200;
+        String expectedBody = "{\"status\":\"Healthy\"}";
+
+        Response response = whenARequestIsMadeTo("/health-check");
+
+        thenResponseShouldSatisfy(expectedStatusCode, expectedBody, response);
+    }
+
+    private Response whenARequestIsMadeTo(String endpoint) {
+        return given().get(endpoint);
+    }
+
+    private void thenResponseShouldSatisfy(int expectedStatusCode, String expectedBody, Response response) {
+        response
+                .then()
+                .statusCode(expectedStatusCode)
+                .contentType(ContentType.JSON)
+                .body(equalTo(expectedBody));
+    }
+
+}
