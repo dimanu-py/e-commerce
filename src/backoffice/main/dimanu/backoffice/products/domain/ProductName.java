0e93208da74b2024a05364779880d81ce0cabd1c
dimanu-py <diegomtz126@gmail.com>
2024-11-30 17:35:44 +0100

    feat(backoffice): create ProductName value object

---
+++ src/backoffice/main/dimanu/backoffice/products/domain/ProductName.java
@@ -0,0 +1,11 @@
+package dimanu.backoffice.products.domain;
+
+import dimanu.shared.domain.StringValueObject;
+
+public class ProductName extends StringValueObject {
+
+    public ProductName(String value) {
+        super(value);
+    }
+
+}
