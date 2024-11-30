7f66c103257f5d4f8bc9c64e838dda3d11d99bb6
dimanu-py <diegomtz126@gmail.com>
2024-11-30 17:38:30 +0100

    feat(backoffice): create ProductDescription value object

---
+++ src/backoffice/main/dimanu/backoffice/products/domain/ProductDescription.java
@@ -0,0 +1,10 @@
+package dimanu.backoffice.products.domain;
+
+import dimanu.shared.domain.StringValueObject;
+
+public class ProductDescription extends StringValueObject {
+
+    public ProductDescription(String value) {
+        super(value);
+    }
+}
