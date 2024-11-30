340f61466edf6a204916d2f71e15d8333cdf7bca
dimanu-py <diegomtz126@gmail.com>
2024-11-30 03:22:13 +0100

    refactor(backoffice): change how products are stored inside InMemoryRepository
    For searching having a hash map would be easier and faster

--- src/backoffice/main/dimanu/backoffice/products/infra/InMemoryProductRepository.java
+++ src/backoffice/main/dimanu/backoffice/products/infra/InMemoryProductRepository.java
@@ -4,16 +4,15 @@ import dimanu.backoffice.products.domain.Product;
 import dimanu.backoffice.products.domain.ProductRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
 
 @Service
 public class InMemoryProductRepository implements ProductRepository {
 
-    private final List<Product> products = new ArrayList<>();
+    private final HashMap<String, Product> products = new HashMap<>();
 
     @Override
     public void save(Product product) {
-        products.add(product);
+        products.put(product.id(), product);
     }
 }
