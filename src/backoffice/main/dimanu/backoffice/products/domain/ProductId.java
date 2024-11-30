15355375ed280de0a1f158bbf7eea7f559fe6c3d
dimanu-py <diegomtz126@gmail.com>
2024-11-30 17:27:23 +0100

    refactor(backoffice): let ProductId extend from Uuid

--- src/backoffice/main/dimanu/backoffice/products/domain/ProductId.java
+++ src/backoffice/main/dimanu/backoffice/products/domain/ProductId.java
@@ -1,37 +1,10 @@
 package dimanu.backoffice.products.domain;
 
-import java.util.Objects;
-import java.util.UUID;
+import dimanu.shared.domain.Uuid;
 
-public class ProductId {
-
-    private final String value;
+public class ProductId extends Uuid {
 
     public ProductId(String value) {
-        ensureIdHasValid(value);
-        this.value = value;
-    }
-
-    private void ensureIdHasValid(String value) {
-        UUID.fromString(value);
-    }
-
-    public String value() {
-        return value;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        ProductId productId = (ProductId) o;
-        return Objects.equals(
-                value,
-                productId.value
-        );
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(value);
+        super(value);
     }
 }
