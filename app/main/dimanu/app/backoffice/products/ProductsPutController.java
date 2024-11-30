0c5c705f1134ac3e684eb0f4bae04dc384735dd7
dimanu-py <diegomtz126@gmail.com>
2024-11-30 02:23:43 +0100

    feat(app-backoffice): define initial contract of ProductsPutController

--- app/main/dimanu/app/backoffice/products/ProductsPutController.java
+++ app/main/dimanu/app/backoffice/products/ProductsPutController.java
@@ -1,5 +1,7 @@
 package dimanu.app.backoffice.products;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/products")
 public class ProductsPutController {
 
+    private final ProductCreator productCreator;
+
+    public ProductsPutController(ProductCreator creator) {
+        this.productCreator = creator;
+    }
+
     @PutMapping("/{id}")
-    public void create(@PathVariable String id, Request request) {
-        throw new UnsupportedOperationException("Not implemented");
+    public ResponseEntity<Void> create(@PathVariable String id, Request request) {
+        productCreator.create(id, request.name(), request.description(), request.price(), request.stock());
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
