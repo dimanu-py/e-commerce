4194179c8465fba96028e7aa0a346a2e613409ff
dimanu-py <diegomtz126@gmail.com>
2024-12-02 10:53:01 +0100

    feat(backoffice): create ProductPrice value object
    Notice that we've decided to model the price as a BigDecimal instead of as a double to avoid precision error

---
+++ src/backoffice/main/dimanu/backoffice/products/domain/ProductPrice.java
@@ -0,0 +1,35 @@
+package dimanu.backoffice.products.domain;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class ProductPrice {
+    private final BigDecimal value;
+
+    public ProductPrice(double value) {
+        ensureIsPositive(value);
+        this.value = BigDecimal.valueOf(value);
+    }
+
+    private void ensureIsPositive(double value) {
+        if (value < 0) {
+            throw new IllegalArgumentException("Price must be positive");
+        }
+    }
+
+    public BigDecimal value() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductPrice that = (ProductPrice) o;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
+    }
+}
