9eac364eaa34e02ab7b0879893d824d54eebf50c
dimanu-py <diegomtz126@gmail.com>
2024-12-02 11:07:29 +0100

    feat(backoffice): create CreateProductCommandMother object

---
+++ src/backoffice/test/dimanu/backoffice/products/application/CreateProductCommandMother.java
@@ -0,0 +1,27 @@
+package dimanu.backoffice.products.application;
+
+public class CreateProductCommandMother {
+
+    public static final String VALID_ID = "8e197c6-0379-4142-acb7-9234f460ca6e";
+    public static final String INVALID_ID = "12345";
+
+    public static CreateProductCommand with_valid_id() {
+        return new CreateProductCommand(
+                VALID_ID,
+                "anyName",
+                "anyDescription",
+                100.0,
+                10
+        );
+    }
+
+    public static CreateProductCommand with_invalid_id() {
+        return new CreateProductCommand(
+                INVALID_ID,
+                "anyName",
+                "anyDescription",
+                100.0,
+                10
+        );
+    }
+}
