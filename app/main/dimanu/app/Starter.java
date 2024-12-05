17401fc1cce162c17ce0f90d0b31c08187975cc7
dimanu-py <diegomtz126@gmail.com>
2024-12-05 15:58:21 +0100

    feat(delivery): add method to scan all the beans defined inside the contexts specified in ComponentScan

--- app/main/dimanu/app/Starter.java
+++ app/main/dimanu/app/Starter.java
@@ -1,9 +1,14 @@
 package dimanu.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Arrays;
 
 @SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
 @ComponentScan({"dimanu.app", "dimanu.backoffice"})
@@ -14,6 +19,17 @@ public class Starter {
                 Starter.class,
                 args
         );
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext context) {
+        return args -> {
+            String[] beansNames = context.getBeanDefinitionNames();
+            Arrays.sort(beansNames);
+            for (String beanName : beansNames) {
+                System.out.println(beanName);
+            }
+        };
     }
 
 }
